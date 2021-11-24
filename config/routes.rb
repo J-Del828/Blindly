638c524191f6b41baa63bcb56417ce73626dd244
SebGustavsson <89755646+SebGustavsson@users.noreply.github.com>
2021-11-24 10:46:16 +0100

    changed routes from matches to decisions and potential_partners

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resource :profile, only: [ :show, :update ]
-  resources :matches, only: [ :create ]
+  resources :potential_partners, only: [ :index ]
+  resources :decisions, only: [ :create ]
   resources :relationships, only: [ :index, :show, :update, :destroy ]
   resources :messages, only: [ :create ]
   resources :users, only: [ :show ]

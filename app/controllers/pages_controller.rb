994c51c6264cebf64a9e915db5ff6df1ed313bcb
Jens Malmlöf <malmlof91@hotmail.com>
2021-11-25 17:57:44 +0100

    created profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
+  def show
+    @user = current_user
+  end
+
   def home
   end
 
   def profile
     @user = current_user
   end
-
-
 
 end

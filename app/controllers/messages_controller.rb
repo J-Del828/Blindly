3c475adaf7fb0fea77cabf5186eae27aad1e4dcb
SebGustavsson <89755646+SebGustavsson@users.noreply.github.com>
2021-11-26 22:24:41 +0100

    chat function and link from index to chats implemented, needs more work tho

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+  def create
+    @relationship = Relationship.find(params[:relationship_id])
+    @message = Message.create(message_params)
+    @message.relationship = @relationship
+    @message.partner_2 = current_user
+    if @message.save
+        redirect_to messages_path(@message, anchor: "message-#{@message.id}")
+    else
+      render "chatrooms/show"
+    end
+  end
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,10 @@
 class RelationshipsController < ApplicationController
-
   def index
     @relationships = current_user.relationships
+  end
+
+  def show
+    @relationship = Relationship.find(params[:id])
+    @message = Message.find_by(relationship_id: @relationship.id)
   end
 end

f7024c137e807293ca79bc348e5e583425954463
SebGustavsson <89755646+SebGustavsson@users.noreply.github.com>
2021-11-29 09:38:40 +0100

    chat base logic is there

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,11 @@
 class MessagesController < ApplicationController
-  def new
-    @message = Message.new
-  end
   def create
-    @relationship = Relationship.find(params[:relationship_id])
+    #@relationship = Relationship.find(params[:relationship_id])
     @current_user = current_user
-    @message = @current_user.messages.create(content: message_params[:content, relationship_id: @relationship])
+    @message = Message.create(message_params)
     @message.relationship = @relationship
     if @message.save
-        redirect_to messages_path(@message, anchor: "message-#{@message.id}")
+      redirect_to messages_path(@message, anchor: "message-#{@message.id}")
     else
       render "new"
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
 Relationship.destroy_all
 Match.destroy_all
 User.destroy_all

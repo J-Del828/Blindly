3c23fe8b8c348663673f1ee0e27afcd2453bdd15
Jens Malmlöf <malmlof91@hotmail.com>
2021-11-30 18:08:00 +0100

    edited profile again

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,5 +10,16 @@ class ProfilesController < ApplicationController
 
   def update
 
+    if current_user.update(user_params)
+      redirect_to profile_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit( :age, :preferred_age, :gender, :preferred_gender, :biography, interests: [])
   end
 end

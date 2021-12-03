b0297dca157cab597cf06a59b8df0c024d48eef9
John Delgado <j.delgado828@gmail.com>
2021-12-03 14:47:52 +0100

    fix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,7 +4,7 @@ class RelationshipsController < ApplicationController
   end
 
   def show
-    @relationship = Relationship.find(params[:id)
+    @relationship = Relationship.find(params[:id])
     # @message = Message.find_by(relationship_id: @relationship.id)
     @message = Message.new
   end
